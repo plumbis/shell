9fcb1da40a71c04f4d2af225e89316f06e57893b
Pete Lumbis <plumbis@nvidia.com>
2024-12-12 13:35:27 -0500

    p10k is a zsh file
    Signed-off-by: Pete Lumbis <plumbis@nvidia.com>

--- shell-init.sh
+++ shell-init.sh
@@ -15,5 +15,5 @@ fc-cache -fv
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 cp .zshrc.sh ~/.zshrc.sh
-cp .p10k.sh ~/.p10k.zsh
+cp .p10k.zsh ~/.p10k.zsh
 
